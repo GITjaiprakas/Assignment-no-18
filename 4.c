feeaf35109c32e5174aa16158ff0f9ee2a885af4
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-11-07 12:43:09 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void convert(char s[]){
+    int c=0;
+    while(s[c]!='\n'){
+        if(s[c]==32){
+            s[c]=s[c];
+            c++;
+       }
+       else{
+           s[c]=s[c]-32;
+           c++;
+        }
+    }
+    puts(s);
+}
+int main(){
+    int c=0;
+    char s[40];
+    printf("Enter any string in lower case : ");
+    fgets(s,40,stdin);
+    convert(s);
+    return 0;
+}
